27639bc2e2ec63282f398d1f42a2f2fad9b9ed47
Pallavic5 <pchedge235@gmail.com>
2023-03-16 09:33:56 +0530

    Added: Ability to display BridgeLabz Logo using Property Binding Technique.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld';
+  
+  //Here applying property data binding
+  imgUrl="../assets/Bridgelabz_Logo.jpg";
+
 //Here Using Interpolation Data Binding
   ngOnInit(): void{
     this.title="Hello From Bridgelabz.";

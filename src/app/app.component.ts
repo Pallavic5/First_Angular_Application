abd3e98084f9ccb542a7a0e587069b6482fb3062
Pallavic5 <pchedge235@gmail.com>
2023-03-16 09:53:37 +0530

    Added: Ability to launch BridgeLabz Site in a new Tab on clicking BridgeLabz Logo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,16 @@ export class AppComponent {
   
   //Here applying property data binding
   imgUrl="../assets/Bridgelabz_Logo.jpg";
+  //Here applying event binding
+  url="https://www.bridgelabz.com";
 
 //Here Using Interpolation Data Binding
   ngOnInit(): void{
     this.title="Hello From Bridgelabz.";
+  }
+  //event binding is used to handle the events raised from the DOM like button click, mouse move etc.
+  onClick($event:any){
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
   }
 }

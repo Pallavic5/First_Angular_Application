bd7ece911f746500e7f6abf2bc6499c39718056a
Pallavic5 <pchedge235@gmail.com>
2023-03-17 07:57:08 +0530

    Added: Ability to display the username is the valid input consisting of initial caps and min 3 letter.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,12 @@ export class AppComponent {
 
   //Here applying event binding
   url="https://www.bridgelabz.com";
-
+  
   //Specify userName as String Type in the App Component Model for forms module
   userName: string = "";
+
+  //Applying regex here
+  nameError: string = "";
 
 //Here Using Interpolation Data Binding
   ngOnInit(): void{
@@ -25,5 +28,15 @@ export class AppComponent {
   onClick($event:any){
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
+  }
+//Applying regex on input event binding
+  onInput($event:any){
+    console.log("Change Event Occurred!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect!";
   }
 }
